53a691e779a9544a3373db18c92b6d57fba21fc8
syedramsha <ramsha.syed464@gmail.com>
2024-07-15 15:47:53 +0200

    Feat : Add New Project and No Project functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
 import ProjectsSidebar from "./components/ProjectsSidebar.jsx";
 import NewProject from "./components/NewProject.jsx";
+import NoProjectSelected from "./components/NoProjectSelected.jsx";
+import {useState} from "react";
+
 function App() {
-  return (
-    <main className="h-screen my-8 flex gap-8">
-        <ProjectsSidebar/>
-        <NewProject></NewProject>
-    </main>
-  );
+    const [projectState, setProjectState] = useState({
+        selectedProjectId: undefined,
+        projects: []
+    });
+    const handleProjectSelection = () => {
+        setProjectState(prevState => {
+            return {
+                ...prevState,
+                selectedProjectId: null
+            };
+        });
+    }
+    let content;
+    if (projectState.selectedProjectId === null) {
+        content = <NewProject/>
+    } else if (projectState.selectedProjectId === undefined) {
+        content = <NoProjectSelected onStartAddProject={() => handleProjectSelection()}/>
+    }
+    return (
+        <main className="h-screen my-8 flex gap-8">
+            <ProjectsSidebar onStartAddProject={() => handleProjectSelection()}/>
+            {content}
+        </main>
+    );
 }
 
 export default App;
