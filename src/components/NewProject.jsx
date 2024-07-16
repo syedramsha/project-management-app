0cb06e90b178857e465d27766cc32144530b2896
syedramsha <ramsha.syed464@gmail.com>
2024-07-16 11:40:22 +0200

    Feat : Add New Project and Sidebar

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,20 +1,54 @@
 import Input from "./Input.jsx";
+import {useRef} from "react";
+import Modal from "./Modal.jsx";
+import modal from "./Modal.jsx";
 
-const NewProject = () => {
-    return <div className="w-[35rem] mt-16">
-        <menu className="flex items-center justify-end gap-4 my-4">
-            <li>
-                <button className="text-stone-800 hover:text-stone-950 ">Cancel</button>
-            </li>
-            <li>
-                <button className='px-6 py-2 bg-stone-800 hover:bg-stone-950 text-stone-50'>Save</button>
-            </li>
-        </menu>
-        <div>
-            <Input label="Title"></Input>
-            <Input label="Description" isTextarea></Input>
-            <Input label="Due Date"></Input>
+const NewProject = ({onAdd, onCancel}) => {
+    const modal = useRef()
+    const title = useRef();
+    const description = useRef();
+    const dueDate = useRef();
+    const handleSave = () => {
+        const enteredTitle = title.current.value;
+        const enteredDescription = description.current.value;
+        const enteredDueDate = dueDate.current.value;
+
+        if (enteredTitle.trim() === '' || enteredDescription.trim() === '' || enteredDueDate.trim() === '') {
+            modal.current.open();
+            return;
+        }
+
+        onAdd({
+            title: enteredTitle,
+            description: enteredDescription,
+            dueDate: enteredDueDate
+        })
+
+
+    }
+    return <>
+        <Modal ref={modal} buttonCaption="Close">
+            <h2 className="text-xl font-bold text-stone-700 my-4">Invalid Input</h2>
+            <p className="text-stone-600 mb-4">Oops... looks like you forgot to enter a value</p>
+            <p className="text-stone-600 mb-4">Please make sure you enter correct values for every Input field</p>
+        </Modal>
+        <div className="w-[35rem] mt-16">
+            <menu className="flex items-center justify-end gap-4 my-4">
+                <li>
+                    <button onClick={onCancel} className="text-stone-800 hover:text-stone-950 ">Cancel</button>
+                </li>
+                <li>
+                    <button onClick={() => handleSave()}
+                            className='px-6 py-2 bg-stone-800 hover:bg-stone-950 text-stone-50'>Save
+                    </button>
+                </li>
+            </menu>
+            <div>
+                <Input type="text" ref={title} label="Title"></Input>
+                <Input ref={description} label="Description" isTextarea></Input>
+                <Input type="date" ref={dueDate} label="Due Date"></Input>
+            </div>
         </div>
-    </div>
+    </>
 }
 export default NewProject
